21f46407fa1c9ad3e6f50d28cf1f4b1a7ec68c29
Murilo L. Mattioli <murilolmattioli@gmail.com>
2023-11-05 08:34:01 -0300

    style: remove user select from logo 💄

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Logo from "@/components/Logo";
 export default function Home() {
   return (
     <main className="flex flex-1 flex-col overflow-hidden mt-[64px]">
-      <div className="flex justify-center h-fit mb-8 md:mb-12">
+      <div className="flex justify-center h-fit mb-8 md:mb-12 select-none">
         <Logo />
       </div>
       <DialerForm />
